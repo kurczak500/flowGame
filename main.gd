44eff26c7691fef1bad285d605a1ead265fc4c53
kurczak500 <dwaksmundzki@interia.pl>
2018-03-18 16:25:22 +0100

    wspolne poprawki

--- main.gd
+++ main.gd
@@ -1,7 +1,5 @@
 extends Node
 
-export (PackedScene) var Tank
-
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
@@ -11,9 +9,3 @@ func _ready():
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
 #	pass
-
-func _input(event):
-	if event is InputEventMouseButton:
-		var new_tank = Tank.instance()
-		new_tank.position = get_viewport().get_mouse_position()
-		add_child(new_tank)
