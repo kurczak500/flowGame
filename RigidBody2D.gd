6ec6ff77fbc559d11b3d99bc2b5da5fff2db0e64
Damian290 <d.klapacz@gmail.com>
2018-05-19 15:16:23 +0200

    Merge_all

---
+++ RigidBody2D.gd
@@ -0,0 +1,19 @@
+extends RigidBody2D
+
+export (int) var MAX_SPEED
+export (int) var MIN_SPEED
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+
+
+func _on_Visibility_screen_exited():
+	queue_free()
+	
--- main.gd
+++ main.gd
@@ -1,11 +1,40 @@
 extends Node
 
+export (PackedScene) var Mob
+export (PackedScene) var Water
+
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
 	pass
 
-#func _process(delta):
+func _process(delta):
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
-#	pass
+	pass
+	
+
+
+func _on_MobTimer_timeout():
+	$MobPath/MobSpawnLocation.set_offset(randi())
+	var mob = Mob.instance()
+	add_child(mob)
+	mob.set_position($MobPath/MobSpawnLocation.position)
+	
+	var direction = ($MobPath/MobSpawnLocation.rotation+ PI/2)
+	direction += rand_range(-PI/4,PI/4)
+	mob.set_rotation(direction)
+	
+	mob.set_linear_velocity(Vector2(rand_range(150,250),0). rotated(direction))
+
+
+func _on_WaterTimer_timeout():
+	$WaterPath/WaterSpawLocation.set_offset(randi())
+	var water = Water.instance()
+	add_child(water)
+	water.set_position($WaterPath/WaterSpawLocation.position)
+	
+	var direction = ($WaterPath/WaterSpawLocation.rotation)
+	water.set_rotation(direction)
+	
+	water.set_linear_velocity(Vector2(30,0).rotated(direction))
